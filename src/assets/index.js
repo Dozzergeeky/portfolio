646af049464344883ab8746eb5659774afa7c45f
Debargha Bhattacharjee <d2b4mb@gmail.com>
2023-09-17 23:56:42 +0530

    fix

--- src/assets/index.js
+++ src/assets/index.js
@@ -26,7 +26,7 @@ import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
-import carrent from "./carrent.png";
+import carrent from "./carrent.jpg";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
