5d30ccaf2d35328331858d70b2fe68a73f469685
Debargha Bhattacharjee <d2b4mb@gmail.com>
2023-09-17 23:48:07 +0530

    final

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Dozzer</span>
+            Hi, I'm <span className="text-[#915EFF]">Debargha</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,8 +64,8 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
-            Liron &nbsp;
-            <span className="sm:block hidden"> | 3D Portfolio</span>
+            Dozzer &nbsp;
+            <span className="sm:block hidden"> | Portfolio</span>
           </p>
         </Link>
 
